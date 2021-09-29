20ecbc6b5246b21548c4b276a69c0486e43830dc
Marcial Rivas <marcial.rivas@gmail.com>
2021-09-29 14:41:43 -0500

    0.0.1

---
+++ fe/trademehere/src/components/HeaderMenu.vue
@@ -0,0 +1,39 @@
+<template>
+  <w-flex>
+    <w-toolbar>
+          <div class="title" v-on:click="showDrawer = !showDrawer">Title</div>
+          <div class="spacer"></div>
+          <span class="ml2">Item 1</span>
+          <span class="ml2">Item 2</span>
+          <span class="ml2">Item 3</span>
+      </w-toolbar>
+      <w-drawer
+        v-model="showDrawer"
+        left= "true"
+        @close="showDrawer = false">
+        <w-button
+          class="button--close"
+          @click="showDrawer = false"
+          sm
+          outline
+          round
+          absolute
+          color="primary"
+          icon="wi-cross">
+        </w-button>
+      </w-drawer>
+  </w-flex>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMenu',
+  props: {
+  },
+  data () {
+    return {
+      showDrawer: false
+    }
+  }
+}
+</script>
